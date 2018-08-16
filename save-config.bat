8a7464396c02cf08a948afb66ee5f95b8f119c76
dodong471520 <1012131075@qq.com>
2018-08-16 15:03:43 +0800

    ds

--- save-config.bat
+++ save-config.bat
@@ -3,6 +3,4 @@ set dst_path=I:/软件存档/编程工具/Atom/atom-config
 copy /y "%src_path%\\config.cson" "%dst_path%/config.cson"
 COPY /y "%src_path%\\keymap.cson" "%dst_path%/keymap.cson"
 apm list --installed --bare > "%dst_path%/installed_package.log"
-git add -A
-git commit -ma "ds"
 pause
